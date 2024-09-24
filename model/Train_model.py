7ce38e6256c87a6088c35291467deee9cc91d8a3
MinhTai <phanminhtai23@gmail.com>
2024-09-24 13:43:08 +0700

    tạo thư mục data_processing, model, them file trực quan dữ liệu

--- model/Train_model.py
+++ model/Train_model.py
@@ -6,10 +6,13 @@ from sklearn.metrics import accuracy_score
 import joblib
 from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_excel('Rice2024_cleaned.xlsx', engine='openpyxl')
+df = pd.read_csv('./standardized-rice.csv')
 
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+# df = df.drop('Convex_Area', axis=1)
+
+X = df.iloc[:, 1:].values
+y = df.iloc[:,0:1].values
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
 
@@ -24,5 +27,5 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Độ chính xác mô hình: {accuracy*100:.3f} %")
-joblib.dump(model, 'random_forest_model_rice.pkl')
+# joblib.dump(model, 'random_forest_model_rice.pkl')
 print("Save model successfully: random_forest_model_rice.pkl")
